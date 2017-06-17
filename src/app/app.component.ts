fbc919635b0690c011a87726b3ea48053cc96420
kayode-adechinan <kayode.adechinan@gmail.com>
2017-06-17 15:21:39 -0700

    adding master details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 
 @Component({
   selector: 'app-root',
@@ -12,9 +25,12 @@ export class Hero {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title = 'Tour of Heroes'
-	  hero: Hero = {
-	  id: 1,
-	  name: 'Windstorm'
-	};
+	title: string = 'Tour of Heroes '
+	selectedHero: Hero;
+	heroes = HEROES;
+
+
+	onSelect(hero: Hero): void {
+  		this.selectedHero = hero;
+	}
 }
